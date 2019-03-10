fc09985683731255b2143f8848150fedc493d710
王治愚 <47854754+wzhiyuyu@users.noreply.github.com>
2019-03-10 18:14:07 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 setup(
     name='SPEEM ',
     version='1.0.0'
-    description='Calculate indicators saved as Excel.',
+    description=('Calculate indicators saved as Excel.'),
     author='haoxiang',
     author_email='haoxiangsnr@gmail.com',
     maintainer='haoxiang',
